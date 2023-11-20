287305b9126c80a5f5dca21fcae5ded5150d57dc
Frieda Gerharz <frieda.gerharz@informatik.hu-berlin.de>
2023-11-20 16:55:53 +0100

    accept more graph input formats; create random b values

--- main.cpp
+++ main.cpp
@@ -2,10 +2,12 @@
 #include <cstring>
 #include <filesystem>
 #include <networkit/Globals.hpp>
+#include <networkit/algebraic/AlgebraicGlobals.hpp>
 #include <networkit/auxiliary/Random.hpp>
-#include <networkit/generators/ErdosRenyiGenerator.hpp>
 #include <networkit/graph/Graph.hpp>
 #include <networkit/graph/GraphTools.hpp>
+#include <networkit/io/GraphToolBinaryReader.hpp>
+#include <networkit/io/METISGraphReader.hpp>
 #include <networkit/io/MatrixMarketGraphReader.hpp>
 #include <networkit/matching/BSuitorMatcher.hpp>
 #include <networkit/matching/DynamicBSuitorMatcher.hpp>
@@ -18,43 +20,46 @@ using namespace NetworKit;
 
 std::string operation;
 uint32_t batch_size;
-std::optional<int> num_b;
-std::optional<std::string> path_b;
+std::optional<count> num_b;
+std::optional<std::vector<count>> vec_b;
 template <typename T> std::vector<T> edges;
 Graph G;
 
 std::vector<dur> dyn_rt;
 std::vector<dur> stat_rt;
-double dyn_mean = 0.0;
-double stat_mean = 0.0;
-double dyn_var;
-double stat_var;
+// double dyn_mean = 0.0;
+// double stat_mean = 0.0;
+// double dyn_var;
+// double stat_var;
 
-[[maybe_unused]] count n = 100;
-[[maybe_unused]] double p = 0.5;
+int num_runs = 50;
+count n;
+// limits for the generation of random b values
+[[maybe_unused]] count b_min = 1;
+[[maybe_unused]] count b_max = 10;
 
-void calculateMean() {
-  for (int i = 0; i < dyn_rt.size(); i++) {
-    dyn_mean += dyn_rt.at(i).count();
-    stat_mean += stat_rt.at(i).count();
-  }
-}
+// void calculateMean() {
+//   for (int i = 0; i < dyn_rt.size(); i++) {
+//     dyn_mean += dyn_rt.at(i).count();
+//     stat_mean += stat_rt.at(i).count();
+//   }
+// }
 
-void calculateVariance() {
-  double dyn_sum_diff_sq = 0.0;
-  double stat_sum_diff_sq = 0.0;
+// void calculateVariance() {
+//   double dyn_sum_diff_sq = 0.0;
+//   double stat_sum_diff_sq = 0.0;
 
-  for (int i = 0; i < dyn_rt.size(); i++) {
-    double dyn_diff = dyn_rt.at(i).count() - dyn_mean;
-    double stat_diff = stat_rt.at(i).count() - stat_mean;
+//   for (int i = 0; i < dyn_rt.size(); i++) {
+//     double dyn_diff = dyn_rt.at(i).count() - dyn_mean;
+//     double stat_diff = stat_rt.at(i).count() - stat_mean;
 
-    dyn_sum_diff_sq += dyn_diff * dyn_diff;
-    stat_sum_diff_sq += stat_diff * stat_diff;
-  }
+//     dyn_sum_diff_sq += dyn_diff * dyn_diff;
+//     stat_sum_diff_sq += stat_diff * stat_diff;
+//   }
 
-  dyn_var = dyn_sum_diff_sq / dyn_rt.size();
-  stat_var = stat_sum_diff_sq / stat_rt.size();
-}
+//   dyn_var = dyn_sum_diff_sq / dyn_rt.size();
+//   stat_var = stat_sum_diff_sq / stat_rt.size();
+// }
 
 std::string pluralS(int num) {
   return (num == 1) ? "" : "s";
@@ -63,32 +68,60 @@ std::string fromOrInto(const char *s) {
   return (std::strcmp(s, "insert") == 0) ? "into" : "from";
 }
 
+std::string getFileFormat(const std::string &file) {
+  auto pos = file.find_last_of(".");
+  if (pos != std::string::npos) {
+    return file.substr(pos + 1);
+  }
+  return "";
+}
+
+std::vector<count> generateRandomVector() {
+  std::vector<count> rand_v;
+  rand_v.reserve(n);
+  for (int i = 0; i < n; i++) {
+    rand_v.emplace_back(Aux::Random::integer(b_min, b_max));
+  }
+  return rand_v;
+}
+
 void printUse() {
   std::cerr << "Usage: ./dyn-b-suitor [0] [1] [2] [3]\n"
-            << "[0]: path to mtx graph file or 'random' for using a random "
-               "generated graph\n"
+            << "[0]: path to graph file in MatrixMarket, METIS or graph-tool "
+               "binary format\n"
             << "[1]: operation 'insert' or 'remove'\n"
             << "[2]: batch size\n"
-            << "[3]: constant b value or path to file with n (number of "
-               "nodes in the graph) b-values (only if graph provided)"
-            << std::endl;
+            << "[3]: constant b value or 'random'\n";
 }
 
 bool parseInput(std::vector<std::string> args) {
-  if (std::strcmp(args.at(1).c_str(), "random") == 0) {
-    // Generate a random undirected Graph with n nodes, a chance of p for an
-    // edge  between a pair of nodes and without self-loops. Set a random weight
-    // between 1 and 100 for each edge.
-    G = ErdosRenyiGenerator(n, p, false, false).generate();
-    G = GraphTools::toWeighted(G);
-    G.forEdges([&](node u, node v) {
-      G.setWeight(u, v, Aux::Random::integer(1, 100));
-    });
-  } else {
-    G = MatrixMarketGraphReader{}.read(args.at(1));
+  const auto file = args.at(1);
+  const auto format = getFileFormat(file);
+  if (format.empty()) {
+    printUse();
+    std::cerr << "first argument path must a path to a graph file" << std::endl;
+    return false;
+  } else if (format == "mtx") {
+    G = MatrixMarketGraphReader{}.read(file);
     G.removeSelfLoops();
     G.removeMultiEdges();
+  } else if (format == "gt") {
+    G = GraphToolBinaryReader{}.read(
+        file); // doesn't work with weighted graphs (GraphToolBinaryReader line
+               // 23 weighted=false) therefore adding random weights below
+    G.removeSelfLoops();
+    G.removeMultiEdges();
+    GraphTools::randomizeWeights(G);
+  } else if (format == "graph") {
+    G = METISGraphReader{}.read(file);
+    G.removeSelfLoops();
+    G.removeMultiEdges();
+  } else {
+    printUse();
+    std::cerr << "invalid graph format" << std::endl;
+    return false;
   }
+  n = G.numberOfNodes();
 
   if (args.at(2) != "insert" && args.at(2) != "remove") {
     printUse();
@@ -106,7 +139,7 @@ bool parseInput(std::vector<std::string> args) {
   if (iss >> num) {
     num_b = num;
   } else {
-    path_b = iss.str();
+    vec_b = generateRandomVector();
   }
 
   return true;
@@ -134,7 +167,7 @@ dur edgeRemoval(Graph &G, DynamicBSuitorMatcher &dbsm) {
   return t2 - t1;
 }
 
-template <typename EdgeType> void runComparison(Graph &G, EdgeType b) {
+template <typename BType> void runComparison(Graph &G, BType &b) {
   // Select m edges of the graph, remove them but put them into edges for
   // later insertion. This will make sure that the graph is valid.
   for (auto j = 0; j < batch_size; j++) {
@@ -146,7 +179,8 @@ template <typename EdgeType> void runComparison(Graph &G, EdgeType b) {
     G.removeEdge(u, v);
   }
 
-  DynamicBSuitorMatcher dbsm(G, b.value());
+  DynamicBSuitorMatcher dbsm(G, b);
+
   dbsm.run();
 
   dur dyn_t = (std::strcmp(operation.c_str(), "insert") == 0)
@@ -156,7 +190,7 @@ template <typename EdgeType> void runComparison(Graph &G, EdgeType b) {
   const auto dm = dbsm.getBMatching();
   const auto dwm = dm.weight(G);
 
-  BSuitorMatcher bsm(G, b.value());
+  BSuitorMatcher bsm(G, b);
 
   const auto t3 = high_resolution_clock::now();
   bsm.run();
@@ -167,42 +201,60 @@ template <typename EdgeType> void runComparison(Graph &G, EdgeType b) {
   const auto sm = bsm.getBMatching();
   const auto wm = sm.weight(G);
 
-  assert(dwm == wm);
+  assert(std::abs(dwm - wm) < FLOAT_EPSILON);
 
   dyn_rt.emplace_back(dyn_t);
   stat_rt.emplace_back(stat_t);
 }
 
 int main(int argc, char *argv[]) {
+  if (argc != 5) {
+    printUse();
+    return 1;
+  }
   std::cout << "Start runtime comparison for the dynamic " << argv[4]
             << "-matching: " << argv[2] << " " << argv[3] << " edge"
             << pluralS(std::atoi(argv[3])) << " " << fromOrInto(argv[2])
             << " graph " << std::filesystem::path(argv[1]).filename()
             << std::endl;
-  if (argc != 5) {
-    std::cout << argc << std::endl;
-    printUse();
-    return 1;
-  }
+  std::cout << std::endl;
+
   Aux::Random::setSeed(0, true);
-  // Graph G;
 
   if (!parseInput(std::vector<std::string>(argv, argv + argc))) {
     return 1;
   }
 
-  for (int i = 0; i < 10; i++) {
+  std::cout << "Start comparison for the dynamic " << argv[4]
+            << "-matching: " << operation << " " << batch_size << " edge"
+            << pluralS(batch_size) << " " << fromOrInto(operation) << " graph "
+            << std::filesystem::path(argv[1]).filename() << " with "
+            << G.numberOfNodes() << " nodes and " << G.numberOfEdges()
+            << " edges." << std::endl;
+  std::cout << std::endl;
+
+  for (int i = 0; i < num_runs; i++) {
     Aux::Random::setSeed(i, true);
-    num_b.has_value() ? runComparison(G, num_b) : runComparison(G, path_b);
+    num_b.has_value() ? runComparison(G, num_b.value())
+                      : runComparison(G, vec_b.value());
   }
 
-  calculateMean();
-  calculateVariance();
+  // calculateMean();
+  // calculateVariance();
 
-  std::cout << "(Dynamic) Adding " << batch_size << " edges took " << dyn_mean
-            << " s on average.\n";
-  std::cout << "(Static)  Running suitor took  " << stat_mean
-            << " s on average.\n";
+  // std::cout << "(Dynamic) Adding " << batch_size << " edges took " <<
+  // dyn_mean << "s on average.\n";
+  std::cout << "(Dynamic) runtimes [s] per run:\n";
+  for (auto r : dyn_rt) {
+    std::cout << r.count() << std::endl;
+  }
+  std::cout << std::endl;
+  // std::cout << "(Static) Running b-suitor took " << stat_mean << "s on
+  // average.\n";
+  std::cout << "(Static) runtimes [s] per run:\n";
+  for (auto r : stat_rt) {
+    std::cout << r.count() << std::endl;
+  }
 
   return 0;
 }
